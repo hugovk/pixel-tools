1cc38a260a4d226081a9e7d500198eb3561747ee
hugovk <hugovk@users.noreply.github.com>
2014-07-02 23:56:17 +0300

    Make a collage of photos inspired by Nana & Felix's Kanta|Väestö

---
+++ kantavaesto.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Make a collage of photos inspired by Nana & Felix's Kanta|Väestö
+http://www.nana-felix.com/
+https://www.facebook.com/nanaandfelix/
+"""
+from __future__ import print_function
+import argparse
+import glob
+from PIL import Image
+
+
+def kantavaesto(inspec, outfile):
+
+    files = glob.glob(inspec)
+
+    im = Image.open(files[0])
+
+    print(len(files), im.size)
+    w, h = im.size
+    left = 0
+    upper = 0
+    right = w
+    lower = h
+
+    # How much smaller should the next image be cropped by?
+    delta = float(im.size[0] / 2) / len(files)
+    print(delta)
+
+    last_bbox = None
+
+    for file in files[1:]:
+        left += delta
+        upper += delta
+        right -= delta
+        lower -= delta
+        bbox = (int(left), int(upper), int(right), int(lower))
+        if last_bbox != bbox:
+            last_bbox = bbox
+            print(bbox)
+            next = Image.open(file).crop(bbox)
+#             next.show()
+            im.paste(next, bbox)
+#             im.show()
+
+    im.show()
+    print("Saving to", outfile)
+    im.save(outfile, quality=100)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Make a collage of photos inspired by "
+        "Nana & Felix's Kanta|Väestö",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '-i', '--inspec', default='*.jpg',
+        help='Input image file spec, must all be the same size')
+    parser.add_argument(
+        '-o', '--outfile',  default='kantavaesto.jpg',
+        help='Output filename')
+    args = parser.parse_args()
+    print(args)
+
+    try:  # Optional, http://stackoverflow.com/a/1557906/724176
+        import timing
+    except:
+        pass
+
+    kantavaesto(args.inspec, args.outfile)
+
+# End of file
