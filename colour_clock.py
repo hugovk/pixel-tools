d152a7af43e25a5c5f9af2854033b775e6d225e9
Hugo <hugovk@users.noreply.github.com>
2018-01-28 11:35:43 +0200

    Remove unnecessary list comprehensions: 'sorted' and 'sum' can take a generator

--- colour_clock.py
+++ colour_clock.py
@@ -126,9 +126,9 @@ def colorz(filename, n=3):
 
 
 def euclidean(p1, p2):
-    return sqrt(sum([
+    return sqrt(sum(
         (p1.coords[i] - p2.coords[i]) ** 2 for i in range(p1.n)
-    ]))
+    ))
 
 
 def calculate_center(points, n):
